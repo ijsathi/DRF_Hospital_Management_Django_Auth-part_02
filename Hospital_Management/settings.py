2f902d1680c2707fd127353ce8d99e47515a545b
unknown <ijsathi2001rs@gmail.com>
2024-07-13 12:05:33 +0600

    allow host

--- Hospital_Management/settings.py
+++ Hospital_Management/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-
+CSRF_TRUSTED_ORIGINS = ["https://drf-hospital-management.onrender.com", "https://*.127.0.0.1"]
 
 # Application definition
 
